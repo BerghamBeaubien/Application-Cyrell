c59ce6f3094fcc7babdb7bff6c4af1e48ea1860b
Mouad Khalladi <157147306+BerghamBeaubien@users.noreply.github.com>
2025-04-01 15:30:36 -0400

    -Derniere vers avant version temporaire pour PAL

--- obj/Debug/net48/Application Cyrell.AssemblyInfo.cs
+++ obj/Debug/net48/Application Cyrell.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application Cyrell")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ace25d089cb790f376890814f77848b55f47ce23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08d146530da4197b89465e92ea68939533e84412")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application Cyrell")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application Cyrell")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
